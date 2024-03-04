85c75cddf534616776891f8934444d65ba097359
Kuson Ta <115791113+1XKuson@users.noreply.github.com>
2024-03-05 01:18:19 +0700

    deploy 1.2.0

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://3.26.39.236:8080'
     // baseURL: 'https://catfact.ninja/fact'
 });
